e2d19d4404119dbf35dd405973a2f217c0cb2ce9
aahana99 <menon.anju@gmail.com>
2016-04-21 15:54:22 -0600

    add mycode

---
+++ mycode.R
@@ -0,0 +1,11 @@
+names(mtcars)
+test <- t.test(mpg ~ am, data= mtcars, var.equal = FALSE, paired=FALSE ,conf.level = .95)
+result <- data.frame( "t-statistic"  = test$statistic, 
+                      "df" = test$parameter,
+                      "p-value"  = test$p.value,
+                      "lower CL" = test$conf.int[1],
+                      "upper CL" = test$conf.int[2],
+                      "automatic mean" = test$estimate[1],
+                      "manual mean" = test$estimate[2],
+                      row.names = "")
+kable(x = round(result,3),align = 'c')
